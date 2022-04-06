ed9864d84f1a3710e847765d3f9415f1aaeb9eb8
jeong woo <jeong woo@DESKTOP-E819GQT>
2022-04-06 21:22:53 +0900

    초기 커밋.

---
+++ 20220406/소스.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+
+
+
+class Array {
+	const int dim;
+	int* size;
+
+	struct Address {
+		int level;
+
+		void* next;
+
+	};
+
+	Address* top;
+
+public:
+	Array(int dim, int* array_size) :dim(dim) {
+		size = new int[dim];
+		for (int i = 0; i < dim; i++) {
+			size[i] = array_size[i];
+		}
+	}
+	void initialize_address(Address* current) {
+		if (!current)	return;
+
+		if (current->level == dim - 1) {//종료조건
+			current->next = new int[size[current->level]];
+			return;
+		}
+
+		current->next = new Address[size[current->level]];
+
+		for (int i = 0; i != size[current->level]; i++) {//다음단계
+			(static_cast<Address*>(current->next) + i)->level = current->level + 1;
+
+			initialize_address(static_cast<Address*>(current->next) + 1);
+		}
+	}
+};
+
